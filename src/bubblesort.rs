19fa3bd7f3660f89500a3946f667761811089693
Oliver Burt <oliburt@gmail.com>
2021-03-22 10:24:55 +0000

    bubble sort

--- src/bubblesort.rs
+++ src/bubblesort.rs
@@ -1,1 +1,34 @@
+use super::Sorter;
 
+pub struct BubbleSort;
+
+impl Sorter for BubbleSort {
+    fn sort<T>(slice: &mut [T])
+    where
+        T: Ord,
+    {
+        let mut swapped = true;
+        while swapped {
+            swapped = false;
+            for i in 1..slice.len() {
+                if slice[i - 1] > slice[i] {
+                    slice.swap(i - 1, i);
+                    swapped = true;
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::sort;
+    use super::*;
+
+    #[test]
+    fn test_bubble() {
+        let mut things = vec![4, 1, 5, 2, 3];
+        sort::<_, BubbleSort>(&mut things);
+        assert_eq!(things, vec![1, 2, 3, 4, 5]);
+    }
+}
